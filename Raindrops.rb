9e542c6e692c7fbe7d03fc089703b18ee1c7d1dc
Kristina Brown <kristina.frey.frey@gmail.com>
2015-04-01 22:19:04 -0600

    raindrops refactored

--- Raindrops.rb
+++ Raindrops.rb
@@ -1,11 +1,14 @@
 class Raindrops
+  @raindrops = { 3 => "Pling",
+                 5 => "Plang",
+                 7 => "Plong" 
+               }
+  
   def self.convert(num)
-    content = []
-    content << num.to_s if num == 1
-    content << "Pling" if  num%3 == 0
-    content << "Plang" if num%5 == 0
-    content << "Plong" if num%7 == 0
-    content << num.to_i if num.to_i == 12121 || num == 52
-    content.join
+    message = ''
+    @raindrops.each do |number, word|
+      message << word if num%number == 0
+    end
+    message.empty? ? num.to_s : message
   end
 end
